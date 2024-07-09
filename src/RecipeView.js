a26a57c44d6d2c4ccd6e3cdceea0b30f492f0edc
John <robnaratt34@gmail.com>
2024-07-08 21:34:48 -0600

    Fixed img tag

--- src/RecipeView.js
+++ src/RecipeView.js
@@ -3,7 +3,7 @@ function RecipeView({recipe, index, recipeDelete}){
     <tr key={index}>
       <td>{recipe.name}</td>
       <td>{recipe.cuisine}</td>
-      <td><img src={recipe.photo} /></td>
+      <td><img src={recipe.photo} alt="No picture provided" /></td>
       <td className="content_td"><p>{recipe.ingredients}</p></td>
       <td className="content_td"><p>{recipe.preparation}</p></td>
       <td><button name="delete" onClick={recipeDelete}>Delete</button></td>
